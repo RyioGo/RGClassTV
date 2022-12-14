0e4a1be6b6717fcf7c80ccf75d50c43f339c3c84
Ryio <zoeryio@outlook.com>
2022-12-14 16:26:31 +0800

    设置默认站点标题

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ module.exports = {
   publicPath: "./", // 公共路径
   outputDir: "dist", // 'dist', 生产环境构建文件的目录
   assetsDir: "static", // 相对于outputDir的静态资源(js、css、img、fonts)目录
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      console.log(args);
+      args[0].title = process.env.VUE_APP_NAME;
+      return args;
+    });
+  },
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "less",
